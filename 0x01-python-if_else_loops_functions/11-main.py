7386acbcb6802af5ed74d0154ef2f76ce52a3c1d
joshchif3 <pantherchif3@gmail.com>
2024-03-13 15:08:50 -0700

    0.11

---
+++ 0x01-python-if_else_loops_functions/11-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from math import pow as custom_pow
+
+print(custom_pow(2, 2))
+print(custom_pow(98, 2))
+print(custom_pow(98, 0))
+print(custom_pow(100, -2))
+print(custom_pow(-4, 5))
