bfe7e8c691f6e7e0490cda15f7a70f9d763018fe
joshchif3 <pantherchif3@gmail.com>
2024-07-09 12:39:50 -0700

    my file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+# Script that takes in a URL, sends a GET request with a custom header, and displays the body of the response
+
+# Check if the URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Make a curl request to the URL with custom header and store response body in a temporary file
+curl -s -H "X-School-User-Id: 98" "$1"
+
+# Ensure new line after output
+echo ""
+
