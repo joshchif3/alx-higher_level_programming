8ffb04760305637c7992fdfbd2edc44b822fd950
joshchif3 <pantherchif3@gmail.com>
2024-07-09 12:13:39 -0700

    my file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Make a curl request to the URL and pipe the output to wc to count bytes
-curl -sI "$1" | grep -i "content-length" | awk '{print $2}' | tr -d '\r'
-
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
